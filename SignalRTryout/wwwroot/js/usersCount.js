58a91a43855a0856acc2aba91694f3c50b89c272
ezgi buse parlakçı <ebecergen@gmail.com>
2024-03-13 15:30:18 +0300

    Add project files.

---
+++ SignalRTryout/wwwroot/js/usersCount.js
@@ -0,0 +1,28 @@
+﻿var connectionUserCount = new signalR.HubConnectionBuilder().
+    withUrl("/hubs/userhub").build();
+
+connectionUserCount.on("UpdateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+connectionUserCount.on("UpdateTotalUsers", (value) => {
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+function fulfilled() {
+    console.log("User count connection successful");
+    newWindowLoadedOnClient();
+}
+
+function rejected(err) {
+    console.error(err.toString());
+}
+
+connectionUserCount.start().then(fulfilled, rejected);
